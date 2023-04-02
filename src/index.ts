e43c44226fbdc4ca187ca4e2dc76622d95a63834
Кайдаров Павел <pavel.kaydarov@fabit.ru>
2023-04-02 21:40:14 +0300

    upd api port

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send('Hello brooooo!');
 });
 app.use('/api/users', userRoutes);
